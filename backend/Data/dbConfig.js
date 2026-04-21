de3e10b95a39bbfbef2506a156de9f84c118c164
Xerox-Fox <joyzero23@gmail.com>
2026-04-21 05:18:57 +0300

    DB changed to postgreSQL

--- backend/Data/dbConfig.js
+++ backend/Data/dbConfig.js
@@ -1,14 +1,11 @@
 const { Pool } = require("pg");
-require("dotenv").config();
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === "production"
-        ? { rejectUnauthorized: false }
-        : false
+    ssl: {
+        rejectUnauthorized: false
+    },
+    family: 4 
 });
 
-module.exports = {
-    query: (text, params) => pool.query(text, params),
-    pool
-};
+module.exports = pool;
