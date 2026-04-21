e6484913baa01d2a78440b4709300cab64f4ba00
Xerox-Fox <joyzero23@gmail.com>
2026-04-21 05:35:25 +0300

    DB changed to postgreSQL

--- backend/Data/dbConfig.js
+++ backend/Data/dbConfig.js
@@ -3,9 +3,7 @@ const { Pool } = require("pg");
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false },
-    max: 10,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 10000
+    family: 4
 });
 
 module.exports = pool;
